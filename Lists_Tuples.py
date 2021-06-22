f9d78d6556d024d97bdba0bd6e222d480afa80b0
Filipe Silva <filipe.seixas@protonmail.com>
2021-06-22 19:43:15 +0100

    Lists and Tuples notes added

--- Lists_Tuples.py
+++ Lists_Tuples.py
@@ -1,2 +1,32 @@
 # Let's create a shopping list
 # Syntax [] - name_of_list = ["", "", ""]
+
+shopping_list = ["apples", "eggs", "dark chocolate", "tea", "bread"]
+print(shopping_list)
+# Checking the type of this data with type()
+print(type(shopping_list))
+
+# How can I access list items using the same concept that we learned yesterday INDEXING
+print(shopping_list[1]) # will display eggs
+print(shopping_list[-1]) # will display the last item (bread)
+
+# How can I replace an item in the list
+shopping_list[0] = "mango"
+print(shopping_list)
+# How can I add an item to our list
+shopping_list.append("tuna")
+print(shopping_list)
+
+# Search python docs and find out how to delete an item from list on index 3
+shopping_list.pop(3)
+print(shopping_list)
+
+# We can have multiple data types in the same list
+mix_list = [1, 2, 3, "one", "two", "three"]
+print(mix_list)
+# What are tuples and what is the difference between lists and tuples
+# Tuple syntax () name_of_tuple = ("", "", "")
+essentials = ("paracetamol", "milk", "butter")
+print(essentials)
+print(type(essentials))
+# Lists are MUTABLE and Tuples are IMMUTABLE
